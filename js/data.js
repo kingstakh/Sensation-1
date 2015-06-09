cf61189d472b77f638f342334ab527bf08173b0e
Gary Readhead <gary@roomworks.co.za>
2015-06-09 13:32:25 +0200

    Make Small Changes

--- js/data.js
+++ js/data.js
@@ -237,15 +237,15 @@ sensationApp.factory('PostsData', function(){
 sensationApp.factory('ServerPostsData', function(){
     
     /* (For DEMO purposes) Local JSON data */
-    var data = { url: 'json/serverposts&' };
-    
+/*    var data = { url: 'json/serverposts&' };
+*/    
     /* Set your URL as you can see in the following example */
     /* NOTE: In case of the default permalinks, you should add '&' at the end of the url */
-    // var data = { url: 'YourWordpressURL/?json=get_recent_posts&' };
+//     var data = { url: 'http://roomworksmedia.co.za/?json=get_recent_posts&' };
     
     /* With user-friendly permalinks configured */
     /* NOTE: In case of the user-friendly permalinks, you should add '?' at the end of the url */
-    // var data = { url: 'YourWordpressURL/api/get_recent_posts?' };
+     var data = { url: 'http://roomworksmedia.co.za/api/get_recent_posts?' };
     
     return data;
 });
